981515b3171cdbe4412318e2d24508bca30b4a65
zhxh <zhxhcoder@gmail.com>
2018-08-08 19:50:26 +0800

    SplashActivity

--- app/src/main/java/com/zhxh/xjetpack/SplashActivity.kt
+++ app/src/main/java/com/zhxh/xjetpack/SplashActivity.kt
@@ -2,13 +2,13 @@ package com.zhxh.xjetpack
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_splash.*
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
